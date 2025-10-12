b090d9a59e7c5c726c49faaee742dff6fc1277bf
ayushi-309 <tayushi751@gmail.com>
2025-10-12 20:09:56 +0530

    change

--- react_country_detail/src/App.jsx
+++ react_country_detail/src/App.jsx
@@ -1,3 +1,29 @@
+import { createBrowserRouter } from "react-router-dom";
+import "./App.css";
+import { About } from "./Pages/About";
+import { Contact } from "./Pages/Contact";
+import { Country } from "./Pages/Country";
+import { Home } from "./Pages/Home";
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element:<Home/>
+  },
+  {
+    path:"about",
+    element:<About/>
+  },
+  {
+    path:"contact",
+    element:<Contact/>
+  },
+  {
+    path:"country",
+    element:<Country/>
+  },
+])
+
 const App = () => {
   return <h1>Hello React project</h1>;
 };
